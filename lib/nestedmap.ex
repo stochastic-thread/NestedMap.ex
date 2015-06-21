c447c79fe8f25b59a6dfce986378fa512a2f0f12
Arthur Colle <hello@arthurcolle.com>
2015-06-21 19:23:54 -0400

    Wrapped everything in Agent.update

--- lib/nestedmap.ex
+++ lib/nestedmap.ex
@@ -6,21 +6,19 @@ defmodule NestedMap do
 
   def insert(agent, a, b) do
     dist = Compare.distance(a[:geo], b[:geo])
-    hd = Agent.get(agent, fn hashd -> hashd end)
-    case HashDict.get hd, a[:id] do
-      nil -> ha = hd |> HashDict.put a[:id], (HashDict.new |> HashDict.put b[:id], dist)
-      a_exists -> 
-        ha = hd |> HashDict.put a[:id], (a_exists |> HashDict.put b[:id], dist)
-    end
+    Agent.update(agent, fn hd -> 
+      case HashDict.get hd, a[:id] do
+        nil -> ha = hd |> HashDict.put a[:id], (HashDict.new |> HashDict.put b[:id], dist)
+        a_exists -> 
+          ha = hd |> HashDict.put a[:id], (a_exists |> HashDict.put b[:id], dist)
+      end
 
-    case HashDict.get ha, b[:id] do
-      nil -> hb = ha |> HashDict.put b[:id], (HashDict.new |> HashDict.put a[:id], dist)
-      b_exists -> 
-        hb = ha |> HashDict.put b[:id], (b_exists |> HashDict.put a[:id], dist)
-    end
-
-    Agent.update(agent, fn hashd -> hb end)
-    agent
+      case HashDict.get ha, b[:id] do
+        nil -> hb = ha |> HashDict.put b[:id], (HashDict.new |> HashDict.put a[:id], dist)
+        b_exists -> 
+          hb = ha |> HashDict.put b[:id], (b_exists |> HashDict.put a[:id], dist)
+      end
+    end)
   end
 
   def inspect(agent) do
